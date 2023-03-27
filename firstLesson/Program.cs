03a8ec48f7b47a8dc0b8b9151d1662e75ad8cbce
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-03-27 17:50:22 +0300

    2

--- firstLesson/Program.cs
+++ firstLesson/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace firstLesson
+﻿using System;
+using static System.Console;
+
+namespace firstLesson
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            /*double a, x;
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            // a = Double.Parse(Console.ReadLine());
+            x = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / (3.56));
             
+            
+            WriteLine("x = {0:f5}", x);
+            WriteLine("Hello world");
+            Write("Hi\n");*/
+
+            int number;
+            int digit1, digit2;
+            Write("number = ");
+            number = Convert.ToInt32(ReadLine());
+
+            if ((number < 10) || (number > 99))
+            {
+                WriteLine("ERROR");   
+                return;
+            }
+
+            digit1 = number / 10;
+            digit2 = number % 10;
+                
+            if (digit1 + digit2 > 9)
+            {
+                WriteLine("number = {0}", digit1 + digit2);
+            }
+            else
+            {
+                WriteLine("<=9");
+            }
+            
+            ReadKey();
         }
     }
 }
