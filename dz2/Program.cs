75472fc4d164c3533231dd57d2298f261b008b0a
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-03 09:43:37 +0300

    DZ2_29.03.2023

---
+++ dz2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace dz2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введіть рядок: ");
+            string inputString = Console.ReadLine();
+
+            int currentCount = 1;
+            int maxCount = 1;
+
+            if (inputString != null)
+                for (int i = 1; i < inputString.Length; i++)
+                {
+                    if (inputString[i] == inputString[i - 1])
+                    {
+                        currentCount++;
+                        if (currentCount > maxCount)
+                        {
+                            maxCount = currentCount;
+                        }
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+                }
+
+            Console.WriteLine("Найбільша кількість символів, що йдуть підряд: " + maxCount);
+        }
+    }
+}
