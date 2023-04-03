ce0633701beb7d33e655b1933a0db5b26a37d1e1
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-03 18:56:18 +0300

    lessons 3 and 4

---
+++ lesson3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+namespace lesson3
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            /*string s1 = "Hello world!";
+            char c = s1[0];
+            Console.WriteLine(c);
+
+            string s2 = "1234";
+            string s3 = "567";
+            
+            s2 = String.Concat(s2, s3);
+            Console.WriteLine(s2);
+
+            s2 = "abcd" + "efg";
+            Console.WriteLine(s2);
+
+            s2 = "Добрий день!";
+            s3 = s2.ToUpper();
+            Console.WriteLine(s3);
+
+            string s4 = "abcdefghi abcd def abc";
+            string s5 = s4.Replace("abc", "AAAAA");
+            Console.WriteLine(s5);
+
+            string s6 = "0123456789";
+            s4 = s6.Insert(2, "AAAAA");
+            Console.WriteLine(s4);
+
+            s6 = "0123456789";
+            s4 = s6.Remove(3, 4);
+            Console.WriteLine(s4);
+            s6 = s6.Insert(0, "+");
+            Console.WriteLine(s6);
+
+            string s7 = "It sddsadad";
+            bool f_is1 = s7.Contains("It");
+            Console.WriteLine("f_is1 = " + f_is1);
+
+            string s8 = "abc,def;dsfsdf sdfs. sdf";
+            char[] chars = {' ', ',', '.', ';' };
+            string[] AS;
+            AS = s8.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < AS.Length; i++)
+            {
+                Console.WriteLine(AS[i]);
+            }
+
+            string[] AS2 = { "abc", "def", "ghij", "op" };
+            s8 = String.Join("+", AS2);
+            Console.WriteLine(s8);*/
+
+            /*string s;
+            int x = 225;
+            double y = 12.33785;
+            
+            s = String.Format("Integer: {0,15:c}", x);
+            Console.WriteLine(s);
+            
+            s = String.Format("Hexadecimal: {0:x}", x);
+            Console.WriteLine(s);
+            
+            s = String.Format("Real: {0,2:f3}", y);
+            Console.WriteLine(s);
+
+            s = String.Format("Exponental: {0:E}", y);
+            Console.WriteLine(s);*/
+
+            StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder stringBuilder2 = new StringBuilder(10);
+            StringBuilder stringBuilder3 = new StringBuilder("Hello, world!");
+
+            stringBuilder2 = new StringBuilder("abcd");
+
+            stringBuilder2[0] = '+';
+            Console.WriteLine(stringBuilder2);
+
+            stringBuilder3.Append("!!");
+            Console.WriteLine(stringBuilder3);
+
+            StringBuilder stringBuilder4 = new StringBuilder("asdasdasd");
+            stringBuilder = stringBuilder4.Insert(0, 2.88);
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder4 = new StringBuilder("0123456789");
+            stringBuilder4.Remove(3, 4);
+            Console.WriteLine(stringBuilder4);
+
+            StringBuilder stringBuilder5 = new StringBuilder("asfafs asfasf asfasf");
+            stringBuilder5.Replace("asfasf", "AAAAA");
+            Console.WriteLine(stringBuilder5);
+
+            StringBuilder stringBuilder6 = new StringBuilder("abcd");
+            char[] AC = new char[10];
+            stringBuilder6.CopyTo(0, AC, 0, stringBuilder6.Length);
+            Console.WriteLine(AC);
+
+            StringBuilder stringBuilder7 = new StringBuilder("abcd");
+            String str;
+            str = stringBuilder7.ToString();
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson4/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace lesson4
+{
+    internal class Program
+    {
+        class Student
+        {
+            private string name = "";
+            private int code = 0;
+            private string group = "";
+            private double rank = 0;
+            
+            public void Input()
+            {
+                Console.Write("name = ");
+                name = Console.ReadLine();
+            
+                Console.Write("Code = ");
+                code = Convert.ToInt32(Console.ReadLine());
+            
+                Console.Write("group = ");
+                group = Console.ReadLine();
+            
+                Console.Write("rank = ");
+                rank = Double.Parse(Console.ReadLine());
+            }
+
+            public void Output()
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("name = " + name);
+                Console.WriteLine("code = " + code);
+                Console.WriteLine("group = " + group);
+                Console.WriteLine("rank = " + rank);
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public int GetCode()
+            {
+                return code;
+            }
+
+            public string GetGroup()
+            {
+                return group;
+            }
+
+            public double GetRank()
+            {
+                return rank;
+            }
+
+            public void SetName(string _name)
+            {
+                name = _name;
+            }
+            
+            public void SetCode(int _code)
+            {
+                code = _code;
+            }
+            
+            public void SetGroup(string _group)
+            {
+                group = _group;
+            }
+            
+            public void SetRank(double _rank)
+            {
+                rank = _rank;
+            }
+        }
+
+        public static void Main()
+        {
+            /*uint n;
+            Console.Write("n = ");
+            n = Convert.ToUInt32(Console.ReadLine());
+            Student[] students;
+            students = new Student[n];
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student();
+            }
+            
+            Console.WriteLine("Enter students: ");
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].Input();
+                Console.WriteLine("-----------------------------");
+            }
+
+            Console.WriteLine("Entered data: ");
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].Output();
+                Console.WriteLine("--------------------------");
+            }*/
+
+            long[] l = new long[10];
+            /*Random random = new Random();
+            for (int i = 0; i < l.Length; i++)
+            {
+                l[i] = random.Next(10, 20);
+            }
+
+            foreach (var t in l)
+            {
+                Console.WriteLine(t);
+            }
+
+            long sum = 0l;
+            for (int i = 0; i < l.Length; i++)
+            {
+                sum += l[i] * l[i];
+            }
+
+            Console.WriteLine("sum = " + sum);*/
+
+            for (int i = 0; i < l.Length; i++)
+            {
+                l[i] = i * i;
+            }
+
+            long[] l2;
+            l2 = new long[l.Length];
+            
+            l.CopyTo(l2, 0);
+            for (int i = 0; i < l2.Length; i++)
+            {
+                Console.Write(l2[i] + "  ");
+            }
+            Console.WriteLine();
+            
+            Console.ReadKey();
+        }
+    }
+}
