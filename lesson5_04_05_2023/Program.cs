ae95ce5a4f829994b8b531800e92542542d74aab
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-05 17:28:57 +0300

    5

--- lesson5_04_05_2023/Program.cs
+++ lesson5_04_05_2023/Program.cs
@@ -15,6 +15,36 @@ namespace lesson5_04_05_2023
             Console.WriteLine("capacity = " + capacity);
             Console.WriteLine("maxcapacity = " + maxcapacity);
             
+            // Багатовимірні масиви - це двохвимірні,
+            // тривимірні і т.д. масиви.
+            
+
+            // 1. Оголошення тривимірного масиву
+            ulong[,,] A;
+            
+            // 2. Виділення пам'яті
+            A = new ulong[3, 4, 5];
+            
+
+            // 3. Заповнити значеннями
+            for (int i = 0; i < A.GetLength(0); i++)
+            for (int j = 0; j < A.GetLength(1); j++)
+            for (int k = 0; k < A.GetLength(2); k++)
+                A[i, j, k] = (ulong)(i + j + k);
+            
+            // 4. Вивести на екран
+            Console.WriteLine("A:");
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                // Вивід таблиці
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    for (int k = 0; k < A.GetLength(2); k++)
+                        Console.Write(A[i, j, k] + "  ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("-----------------------");
+            }
             
             Console.ReadKey();
         }
