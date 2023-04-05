f860d3054a5fb9292417f432dabb6c7ad091b4b8
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-05 16:53:39 +0300

    5

---
+++ lesson5_04_05_2023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace lesson5_04_05_2023
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+             
+            
+            Console.ReadKey();
+        }
+    }
+}
