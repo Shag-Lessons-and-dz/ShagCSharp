0a051940ea74837c613fd00227619486aaf33013
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-05 17:08:05 +0300

    5

--- lesson5_04_05_2023/Program.cs
+++ lesson5_04_05_2023/Program.cs
@@ -5,7 +5,7 @@ namespace lesson5_04_05_2023
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             StringBuilder stringBuilder = new StringBuilder("abcdefghi");
             int capacity, maxcapacity;
