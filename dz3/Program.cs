d71bb3c6f330b9f4d076751ebd972c5121e6b1ea
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-07 17:23:13 +0300

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace dz3
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] AD = new double[n];
+            Random random = new Random();
+            Console.Write("AD => ");
+            for (int i = 0; i < AD.Length; i++)
+            {
+                AD[i] = random.NextDouble() * 10.0 - 5.0;
+                Console.Write(" "+ AD[i].ToString("F3")+ " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("min = " + AD.Min().ToString("F3"));
+            Console.WriteLine("max = " + AD.Max().ToString("F3"));
+
+            Console.ReadKey();
+        }
+    }
+}
