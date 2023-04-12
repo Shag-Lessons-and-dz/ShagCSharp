1bba82710c728f28ac4fe72a7561569c9079a690
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-04-12 16:30:39 +0300

    6 and 7

---
+++ lesson6_4_10_2023/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace lesson6_4_10_2023
+{
+    class Team
+    {
+        private string name;  // назва команди
+        private int games;    // к-сть ігор
+        private int win;      // к-сть перемог
+        private int draw;     // к-сть нічиїх
+        private int lose;     // к-сть програшів
+
+
+
+        // Конструктор з 5 параметрами
+        public Team(string name, int games, int win, int draw, int lose)
+        {
+            this.name = name;
+            this.games = games;
+            this.win = win;
+            this.draw = draw;
+            this.lose = lose;
+        }
+
+
+
+        // Конструктор з 1 параметром
+        public Team(string name) : this(name, 0, 0, 0, 0)
+        { }
+
+        // Метод тестування
+        public void Print()
+        {
+            Console.Write(name + " => ");
+            Console.Write("W-{0}  ", win);
+            Console.Write("D-{0}  ", draw);
+            Console.Write($"L-{lose}  ");
+            Console.Write("G-" + games + "  ");
+            Console.Write($"P-{CalcPoints()}");        
+            Console.WriteLine();
+        }
+
+        public string GetName() { return name; }
+        public void SetName(string name) { this.name = name; }
+
+        // Метод CalcPoints
+        public int CalcPoints()
+        {
+            return win * 3 + draw;
+        }
+
+        // Статичний метод 
+        /*
+        public static int CalcPointsStatic(Team obj)
+        {
+
+
+ 
+
+        } 
+        */
+
+
+
+        // Властивості - це елементи класу, які реалізують 
+        // зручний та наочний вигляд доступу до елементів або 
+        // проведення якихось обчислень.
+        // Виглядають як методи, тільки без дужок.
+
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }  // тут value - контекстно-залежне слово
+        }
+
+
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team tm = new Team("Barselona", 8, 5, 2, 1);
+            tm.Print();
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson7_04_12_2023/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+namespace lesson7_04_12_2023
+{
+    
+    // Клас, який демонструє способи
+
+// передачі аргументів у метод.
+
+    class DemoPassingParameters
+
+    {
+
+// Метод, який отримує тип-значення за значенням.
+
+// Тут робиться копія аргументу.
+
+        public void PassingValueType(int a)
+
+        {
+
+            a = 100; // спроба змінити значення
+
+        }
+
+// Метод, який отримує параметром тип Integer
+
+        public void PassingObjectType(Integer obj)
+
+        {
+
+// спроба змінити значення за посиланням obj
+
+            obj.value = 100;
+
+        }
+
+// Метод, який отримує параметром ref-посилання
+
+        public void PassingValueTypeRef(ref int a) // таке як в C++ знак &
+
+        {
+
+            a = 100;
+
+        }
+
+// Метод, який отримує ref-посилання на об'єкт
+
+        public void PassingObjectTypeRef(ref Integer[] obj)
+
+        {
+
+            obj = new Integer[5];
+
+            for (int i = 0; i < obj.Length; i++)
+
+            {
+
+                obj[i] = new Integer();
+
+                obj[i].value = i + i;
+
+            }
+
+        }
+
+    }
+
+// Деякий клас
+
+    class Integer
+
+    {
+
+        public int value;
+
+    }
+    
+    internal class Program
+    {
+        
+        
+        public static void Main(string[] args)
+        {
+            /*
+
+            // Тема: Передача аргументів у функцію.
+
+            // Існує 3 способи передачі аргументу в метод:
+
+            - за значенням;
+
+            - як ref-посилання;
+
+            - як out-посилання.
+
+            */
+
+            DemoPassingParameters demoObj = new DemoPassingParameters();
+
+            // 1. Передача типу-значення у метод за значенням
+
+            int t = 200;
+
+            demoObj.PassingValueType(t); // у методі робиться спроба зміни аргументу t
+
+            Console.WriteLine("t = " + t);
+
+            // t = 200 - при передачі за значенням робиться копія змінної-аргументу.
+
+            // При цьому способі оригінал не змінюється.
+
+            // 2. Передача типу-посилання у метод за значенням
+
+            Integer objInt = new Integer();
+
+            objInt.value = 200;
+
+            demoObj.PassingObjectType(objInt); // objInt.value = 100 - зміна даних об'єкту
+
+            Console.WriteLine("objInt.value = " + objInt.value);
+
+            // У цьому випадку копіюється тільки посилання objInt.
+
+            // Виходить, що в методі PassingObjectType() ми працюємо
+
+            // з копією посилання, тобто з копією адреси за якою розміщуються
+
+            // дані (value). Що оригінал, що копія адреси все одно вказують
+
+            // на ту саму область пам'яті, яку можна змінити як з
+
+            // оригіналу, так і з копії. Тому дані за адресою (value) були змінені.
+
+            // ---------------- ref --------------------------------
+
+            // 3. Передача ref-аргументу в метод, аргумент - тип значення
+
+            t = 200;
+
+            demoObj.PassingValueTypeRef(ref t);
+
+            Console.WriteLine("t = " + t); // t = 100
+
+            // 4. Передача ref-аргументу в метод, аргумент - тип посилання
+
+            // Для типу-посилання можна змінити саме посилання
+
+            Integer[] AI = new Integer[3];
+
+            for (int i = 0; i < AI.Length; i++)
+
+            {
+
+                AI[i] = new Integer();
+
+                AI[i].value = i * i;
+
+            }
+
+            // Тут відбувається зміна самого масиву AI
+
+            demoObj.PassingObjectTypeRef(ref AI);
+
+            Console.Write("AI => ");
+
+            for (int i=0; i<AI.Length; i++)
+
+            {
+
+                Console.Write(AI[i].value + " ");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
