bc77a7e6de20d6d83ec90b3a63a970a369b83347
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-05-03 17:47:59 +0300

    62

---
+++ lesson11_05.03.2023/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson11_05._03._2023
+{
+    public class Outer
+    {
+        // Вложенный класс
+        public class Inner
+        {
+            public int a;
+        }
+
+        Inner objInner = new Inner();
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Создать объект вложенного класса
+            Outer.Inner objInner = new Outer.Inner();
+            objInner.a = 100;
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
