6ab208343cc1cb8a504b5c62f2f2f1a2cfdfbfdb
MoonShine <119930416+Lesbica@users.noreply.github.com>
2023-05-08 18:24:52 +0300

    sd

---
+++ lesson12_08_05_2023/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace lesson12_08_05_2023
+{
+    class A
+    {
+        public void Show()
+        {
+            Console.WriteLine("A.Show()");
+        }
+
+        public void ShowA()
+        {
+            Console.WriteLine("A.ShowA()");
+        }
+    }
+
+    class B : A
+    {
+        public new void Show()
+        {
+            Console.WriteLine("B.Show()");
+        }
+
+        public void ShowB()
+        {
+            Console.WriteLine("B.ShowB()");
+        }
+    } 
+    
+    class C : B
+    {
+        public new void Show()
+        {
+            Console.WriteLine("C.Show()");
+        }
+
+        public void ShowC()
+        {
+            Console.WriteLine("C.ShowCc()");
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ practica5_05_08_2023/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace practica5_05_08_2023
+{
+    class Figure {
+        protected string name;
+
+        public Figure(string name) {
+            this.name = name;
+        }
+
+        public virtual void Display() {
+            Console.WriteLine("Figure: {0}", name);
+        }
+    }
+
+    class Circle : Figure {
+        private double x, y, r;
+
+        public Circle(string name, double x, double y, double r) : base(name) {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+
+        public Circle() : this("Circle", 0, 0, 1) {}
+
+        public double X {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public double R {
+            get { return r; }
+            set { r = value; }
+        }
+
+        public override void Display() {
+            base.Display();
+            Console.WriteLine("X: {0}, Y: {1}, R: {2}", x, y, r);
+        }
+
+        public double Area {
+            get { return Math.PI * r * r; }
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Figure fig = new Figure("Figure");
+            Circle cir = new Circle("Circle", 1, 2, 3);
+
+            fig.Display();
+            cir.Display();
+
+            fig = cir;
+            fig.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
